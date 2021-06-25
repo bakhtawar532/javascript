c53acf71c08ae762a310d87f44c49bbf725b125e
Bakhtawar Shahid <83647449+bakhtawar532@users.noreply.github.com>
2021-06-25 12:05:15 -0700

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+  return(
+ <footer>
+   <p>Copyright by BakhtawarShahid @ 2021{new Date().getFullYear()}
+     </p>
+   </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+ <div className="note">
+   <h1>JavaScript & React js Bootcamp</h1>
+   <p>JavaScript And React js web Dev BootCamp</p>
+   </div>
+  );
+}
+
+export default Info;
